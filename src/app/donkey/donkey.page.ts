2b306e6759b2da88e2c72d6b0a449e019c488c7e
car2617 <125669453+car2617@users.noreply.github.com>
2023-06-23 16:15:38 +0000

    lingual

--- src/app/donkey/donkey.page.ts
+++ src/app/donkey/donkey.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-donkey',
   templateUrl: './donkey.page.html',
   styleUrls: ['./donkey.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,RouterLink]
 })
 export class DonkeyPage implements OnInit {
 
